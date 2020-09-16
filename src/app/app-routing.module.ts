3d59fae534c4586e6e5a963255a80eb7b000a0be
Duilio Gomes <dcg.duiliogomes@gmail.com>
2020-09-15 23:15:40 -0300

    create database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'addresses',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'message/:id',
+    path: 'address',
+    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
+  },
+  {
+    path: 'address/:id',
     loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
   },
   {
---
+++ src/app/core/service/database.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  db: SQLiteObject;
+  databaseName: string = 'address.db';
+
+  constructor(private sqlite: SQLite, private sqlitePorter: SQLitePorter) { }
+
+  async openDatabase(){
+    try {
+      this.db = await this.sqlite.create({ name: this.databaseName, location: 'default' });
+      await this.createDatabase();
+    } catch (error){
+      console.error('Creation Database error', error);
+    }
+  }
+
+  async createDatabase(){
+    const sqlCreateDatabase = this.getCreateTable();
+    const result = await this.sqlitePorter.importSqlToDb(this.db, sqlCreateDatabase);
+    return result ? true : false;
+  }
+
+  getCreateTable(){
+    const sqls = [];
+    sqls.push('CREATE TABLE IF NOT EXISTS address (id integer key AUTOINCREMENT, desciption varchar(255));');
+    return sqls.join('\n');
+  }
+
+  executeSQL(sql: string, params?: any[]){
+    return this.db.executeSql(sql, params);
+  }
+
+}
